261078fbf81af4d196bb750870d0535224cab728
Drongo <andreyradin@gmail.com>
2023-04-09 08:05:41 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число A: ");
+string text1 = Console.ReadLine();
+int number1 = Console.ToInt32(text1);
+Console.Write("Введите число B: ");
+string.text2 = Console.ReadLine();
+int number2 = Console.ToInt32(next2);
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("A является квадратом B");
+}
+else
+{
+    Console.WriteLine("A не является квадратом B");
+}
