6582fcfabe41f494f2190d0bfc0a38ac7aa387c6
Drongo <andreyradin@gmail.com>
2023-04-11 17:35:49 +0300

    DZ_task2

---
+++ DZ_task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a> b){
+    System.Console.WriteLine("Большее число: A, меньшее число: B");
+} 
+else System.Console.WriteLine("Большее число: B, меньшее число: A");
