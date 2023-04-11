9317423e489f7e199de8888a91e5d92702fbac85
Drongo <andreyradin@gmail.com>
2023-04-11 17:10:42 +0300

    DZ_task4

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Введите число: ");
 string text = Console.ReadLine();
-int number = Convert.ToUInt32(text);
+int number = Convert.ToInt32(text);
 int negativeNumber = number * -1;
 while (negativeNumber <= number)
 {
-    console.Write(negativeNamber);
-    console.write(" ");
+    Console.Write(negativeNumber);
+    Console.Write(" ");
     negativeNumber++;
 }
